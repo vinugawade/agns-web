fcebfc20a25ff6a55ab158b89c067385ea4a4401
vinugawade <vinulike11@gmail.com>
2022-12-04 16:53:54 +0530

    feat: add trusted hosts

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -26,6 +26,11 @@ include __DIR__ . "/settings.pantheon.php";
 // $settings['skip_permissions_hardening'] = TRUE;
 $settings['config_sync_directory'] = '../config';
 
+$settings['trusted_host_patterns'] = [
+  '^agns\.in$',
+  '*'
+];
+
 /**
  * If there is a local settings file, then include it
  */
